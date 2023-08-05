d24b65316905f9a55064bb08d26b3655adc6b303
Yichuan Shen <shen.yichuan@gmail.com>
2023-08-05 02:56:01 +0200

    Fix path

--- wdio.conf.js
+++ wdio.conf.js
@@ -21,7 +21,7 @@ export const config = {
   // then the current working directory is where your `package.json` resides, so `wdio`
   // will be called from there.
   //
-  specs: ["./test_build/*.test.ts"],
+  specs: ["./test_build/*.test.js"],
   // Patterns to exclude.
   exclude: [
     // 'path/to/excluded/files'
